8778a30babb941a146e1c6b82c6b5ab20db9f720
Ayan <ayandgr5@gmail.com>
2023-11-30 12:27:23 +0530

    backend made

---
+++ server.js
@@ -0,0 +1,41 @@
+// server.js
+
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const dotenv = require('dotenv').config();
+const UserDetails = require('./src/landing');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect(process.env.mongoDB, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        console.log('MongoDB connected successfully');
+    })
+    .catch((error) => {
+        console.error('MongoDB connection failed:', error);
+    });
+
+app.post('/user-details', async (req, res) => {
+    try {
+        console.log('Received request:', req.body);
+
+        const user = new UserDetails(req.body);
+        const savedUser = await user.save();
+
+        console.log('User details saved successfully:', savedUser);
+        
+        res.json(savedUser);
+    } catch (error) {
+        console.error('Error saving user details:', error);
+        res.status(500).json({ error: error.message });
+    }
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
---
+++ src/landing.js
@@ -0,0 +1,17 @@
+// src/models/landing.js
+
+const mongoose = require('mongoose');
+
+const landingSchema = mongoose.Schema({
+    name: { type: String, required: true, required: true },
+    phone: { type: Number, required: true, required: true },
+    email: { type: String, unique: true, required: true },
+    treatment: { type: String, required: true, required: true },
+    bodypart: { type: String, required: true, required: true },
+    location: { type: String, required: true, required: true }
+});
+
+
+const UserDetails = mongoose.model("UserDetails", landingSchema)
+
+module.exports = UserDetails;     
