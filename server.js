4e3c45871bdefff888d898f28897231325a6a5bf
Ayan <ayandgr5@gmail.com>
2023-12-07 14:28:54 +0530

    deployed 2

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 // server.js
+
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const dotenv = require('dotenv').config();
-const routes = require('./Routes'); 
+const UserDetails = require('./src/landing');
 
 const app = express();
 app.use(cors());
 app.use(express.json());
-
 const { mongoDB, SERVER_SELECTION_TIMEOUT, SOCKET_TIMEOUT } = process.env;
 
 mongoose.connect(mongoDB, { 
@@ -24,8 +24,21 @@ mongoose.connect(mongoDB, {
     console.error('MongoDB connection failed:', error);
 });
 
-// Use the Routes
-app.use('/', routes);
+app.post('/user-details', async (req, res) => {
+    try {
+        console.log('Received request:', req.body);
+
+        const user = new UserDetails(req.body);
+        const savedUser = await user.save();
+
+        console.log('User details saved successfully:', savedUser);
+        
+        res.json(savedUser);
+    } catch (error) {
+        console.error('Error saving user details:', error);
+        res.status(500).json({ error: error.message });
+    }
+});
 
 const PORT = process.env.PORT || 3000;
 
