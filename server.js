92f8602c3d1e99f5e4f79e20f862a3500499515f
Ayan <ayandgr5@gmail.com>
2023-12-08 12:54:00 +0530

    logger.js added

--- server.js
+++ server.js
@@ -1,28 +1,35 @@
 // server.js
-
 const express = require('express');
 const mongoose = require('mongoose');
+const morgan = require('morgan');
 const cors = require('cors');
-const dotenv = require('dotenv').config();
+const logger = require('./logger');
 const UserDetails = require('./src/landing');
+require('dotenv').config();
 
 const app = express();
-app.use(cors());
 app.use(express.json());
-const { mongoDB, SERVER_SELECTION_TIMEOUT, SOCKET_TIMEOUT } = process.env;
+
+// Set up Morgan middleware for HTTP request logging
+app.use(morgan('combined', { stream: { write: (message) => logger.info(message.trim()) } }));
+
+// Enable CORS
+app.use(cors());
+
+// MongoDB connection
+const { mongoDB } = process.env;
 
 mongoose.connect(mongoDB, { 
     useNewUrlParser: true, 
-    useUnifiedTopology: true,
-    serverSelectionTimeoutMS: SERVER_SELECTION_TIMEOUT || 30000,
-    socketTimeoutMS: SOCKET_TIMEOUT || 45000,
+    useUnifiedTopology: true 
 })
-.then(() => {
-    console.log('MongoDB connected successfully');
-})
-.catch((error) => {
-    console.error('MongoDB connection failed:', error);
-});
+  .then(() => {
+    logger.info('MongoDB connected successfully');
+  })
+  .catch((error) => {
+    logger.error('MongoDB connection failed:', error);
+  });
+
 
 app.post('/user-details', async (req, res) => {
     try {
@@ -39,6 +46,7 @@ app.post('/user-details', async (req, res) => {
         res.status(500).json({ error: error.message });
     }
 });
+
 
 const PORT = process.env.PORT || 3000;
 
